ff1b2821f8e583516a3d02d5b409cb1d8e4007c3
USER <USER@DESKTOP-GMPMTDL>
2023-03-27 17:48:51 +0900

    03/27

--- DevFox/src/main/java/com/DevFox/biz/user/impl/Test.java
+++ DevFox/src/main/java/com/DevFox/biz/user/impl/Test.java
@@ -2,12 +2,11 @@ package com.DevFox.biz.user.impl;
 
 import java.sql.*;
 import javax.sql.*;
-
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Repository;
 
-
-
+@Repository
 public class Test {
 
     private DataSource dataSource;
@@ -22,20 +21,18 @@ public class Test {
         }
     }
 
-
     public static void main(String[] args) throws SQLException {
     	ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
     	
     	if (context == null) {
     	    System.out.println("Failed to load applicationContext.xml");
-    	} else {
+    	}else {
     	    System.out.println("Loaded applicationContext.xml successfully");
     	    Test test = context.getBean(Test.class);
     	    System.out.println("빈가져온다");
             test.setDataSource(context.getBean("dataSource", DataSource.class));
             test.testConnection();
     	}
-     
     }
 
     public void setDataSource(DataSource dataSource) {
